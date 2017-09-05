f02980c6571e310152e4ec8d69415a865edf4c3f
Ivan <kyselovivan@gmail.com>
2017-09-05 11:47:24 +0300

    added Sounds class

---
+++ app/src/main/java/com/example/ivan/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.example.ivan.beatbox;
+
+/**
+ * Created by ivan on 9/5/17.
+ */
+
+public class Sound {
+    private String mAssetsPath;
+    private String mName;
+
+    public Sound(String assetsPath) {
+        mAssetsPath = assetsPath;
+        String[] components = assetsPath.split("/");
+        String fileName = components[components.length-1];
+        mName = fileName.replace(".wav","");
+    }
+
+    public String getAssetsPath() {
+        return mAssetsPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
